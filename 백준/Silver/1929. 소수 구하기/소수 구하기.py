eafe58016fea433018e1bb04bf29891e8fbbd84f
이재혁 <93297796+JaeHyeokee@users.noreply.github.com>
2024-12-27 15:43:38 +0900

    [Silver III] Title: 소수 구하기, Time: 3232 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,8 @@
+def isPrimenumber(num):
+    if num == 1: return False
+    for i in range(2, int(num**(1 / 2)) + 1):
+        if num % i == 0: return False
+    return True
+M, N = map(int, input().split())
+for i in range(M, N + 1):
+    if isPrimenumber(i): print(i)
